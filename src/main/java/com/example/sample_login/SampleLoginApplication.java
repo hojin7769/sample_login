e09bc3b5ab121d72e491059475f912b0fc24dad9
hojin7769 <sungsill150@gmail.com>
2023-04-04 10:17:58 +0900

    webapplication 수정

--- src/main/java/com/example/sample_login/SampleLoginApplication.java
+++ src/main/java/com/example/sample_login/SampleLoginApplication.java
@@ -2,12 +2,26 @@ package com.example.sample_login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com"})
+@EnableJpaRepositories( basePackages = "com.example")
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class SampleLoginApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SampleLoginApplication.class, args);
+        String profile = System.getProperty( "spring.profiles.active" , "local" );
+        String envPath = System.getProperty( "spring.config.location", "src/main/resources/env/" + profile + "/" );
+
+        System.setProperty( "spring.profiles.active" , profile );
+        System.setProperty( "spring.config.location" , envPath );
+
+        SpringApplication sp = new SpringApplication( SampleLoginApplication.class );
+        sp.run(args);
+
     }
 
 }
